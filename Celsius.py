42bc0b509a5c548ad9a48ecd2faf63cb512ebea0
loganfreeman <shanhongcheng@hotmail.com>
2016-10-09 17:35:09 -0600

    Create Celsius.py

---
+++ Celsius.py
@@ -0,0 +1,7 @@
+class Celsius(object):
+    def __init__(self, value=0.0):
+        self.value = float(value)
+    def __get__(self, instance, owner):
+        return self.value
+    def __set__(self, instance, value):
+        self.value = float(value)
