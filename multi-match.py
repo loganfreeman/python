a644b02dca465d2d384f6ce44812adccdbf8d6f2
loganfreeman <shanhongcheng@hotmail.com>
2016-10-11 14:50:54 -0600

    Create multi-match.py

---
+++ multi-match.py
@@ -0,0 +1,25 @@
+
+def multi_match(text, *patterns):
+    """Scans through a string for substrings matched some patterns (first-subgroups only).
+    Args:
+        text: A string to be scanned.
+        patterns: Arbitrary number of regex patterns.
+    Returns:
+        When only one pattern is given, returns a string (None if no match found).
+        When more than one pattern are given, returns a list of strings ([] if no match found).
+    """
+
+    if len(patterns) == 1:
+        pattern = patterns[0]
+        match = re.search(pattern, text)
+        if match:
+            return match.group(1)
+        else:
+            return None
+    else:
+        ret = []
+        for pattern in patterns:
+            match = re.search(pattern, text)
+            if match:
+                ret.append(match.group(1))
+        return ret
