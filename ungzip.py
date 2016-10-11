4ddccbb68b3166425dc6c871b878b746d0703c9c
loganfreeman <shanhongcheng@hotmail.com>
2016-10-11 14:54:52 -0600

    Create ungzip.py

---
+++ ungzip.py
@@ -0,0 +1,16 @@
+def ungzip(data):
+    """Decompresses data for Content-Encoding: gzip.
+    """
+    from io import BytesIO
+    import gzip
+    buffer = BytesIO(data)
+    f = gzip.GzipFile(fileobj=buffer)
+    return f.read()
+
+def undeflate(data):
+    """Decompresses data for Content-Encoding: deflate.
+    (the zlib compression is used.)
+    """
+    import zlib
+    decompressobj = zlib.decompressobj(-zlib.MAX_WBITS)
+    return decompressobj.decompress(data)+decompressobj.flush()
