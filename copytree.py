7878db8851cf78518bb759aac62fb1e30781d500
loganfreeman <shanhongcheng@hotmail.com>
2016-10-11 17:15:34 -0600

    Create copytree.py

---
+++ copytree.py
@@ -0,0 +1,33 @@
+def copytree(src, dst, symlinks=False, ignore=None):
+    """
+    This is a contributed re-implementation of 'copytree' that
+    should work with the exact same behavior on multiple platforms.
+    """
+
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+        shutil.copystat(src, dst)
+    lst = os.listdir(src)
+
+    if ignore:
+        excl = ignore(src, lst)
+        lst = [x for x in lst if x not in excl]
+
+    for item in lst:
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+
+        if symlinks and os.path.islink(s): # pragma: no cover
+            if os.path.lexists(d):
+                os.remove(d)
+            os.symlink(os.readlink(s), d)
+            try:
+                st = os.lstat(s)
+                mode = stat.S_IMODE(st.st_mode)
+                os.lchmod(d, mode)
+            except:
+                pass  # lchmod not available
+        elif os.path.isdir(s):
+            copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
